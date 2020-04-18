f5115502a24813ab80930e69f1d2b55c7b4062dc
Gabriel Goffart <gab.goffart@gmail.com>
2020-04-18 13:53:38 -0400

    add abort on pull_request option

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
 import * as core from "@actions/core"
+import { context } from "@actions/github"
 import { exec } from "@actions/exec"
 
-const onError = (error: Error) => {
-   core.setFailed(error.message)
-}
+const onError = (error: Error) => core.setFailed(error.message)
 
 const run = async () => {
    const username = core.getInput("username")
    const password = core.getInput("password")
    const image = core.getInput("image")
    const tag = core.getInput("tag")
+   const onPr = core.getInput("on-pr")
+
+   if (context.eventName === "pull_request" && onPr !== "true") {
+      console.log("Abort due to config")
+      return
+   }
 
    console.log("Login to docker hub")
    await exec(`docker login -u ${username} -p ${password}`)
