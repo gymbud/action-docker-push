d1c1d06c403e3c7b47ef19279b1b97f878bebc22
Gabriel Goffart <gab.goffart@gmail.com>
2020-04-18 13:17:10 -0400

    implement push mechanism

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
-import github from "@actions/github"
-import core from "@actions/github"
-import cp from "child_process"
+import * as github from "@actions/github"
+import * as core from "@actions/core"
+import { exec } from "@actions/exec"
 
-const onError = (error: Error) => {}
+const onError = (error: Error) => {
+   core.setFailed(error.message)
+}
 
-const run = async () => {}
+const run = async () => {
+   const username = core.getInput("username")
+   const password = core.getInput("password")
+   const image = core.getInput("image")
+   const tag = core.getInput("tag")
+
+   console.log("Login to docker hub")
+   await exec(`docker login -u ${username} -p ${password}`)
+
+   console.log("Push to docker hub")
+   await exec(`docker push ${image}:${tag}`)
+
+   console.log("All done!")
+}
 
 process.setUncaughtExceptionCaptureCallback(onError)
 
